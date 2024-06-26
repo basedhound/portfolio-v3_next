ef61081b217c255ab37db53a483a9884a3837146
Jozef S <franck.vukelic@gmail.com>
2024-06-26 18:39:49 +0700

    Update metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,28 +5,29 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>John Carlo Devera | Frontend Developer</title>
-        <link rel="shortcut icon" href="/favicon.png" />
-        <meta name="description" content="John Carlo Devera | Frontend Developer" />
+        <title>Frank Vukelić | Web Developer</title>
+        <link rel="shortcut icon" href="/assets/favicon.png" />
+        <meta name="description" content="Frank Vukelić | Web Developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="John Carlo Devera | Frontend Developer" />
-        <meta name="description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate." />
+        <meta property="og:title" content="Frank Vukelić | Web Developer" />
+        <meta name="description" content="Hey! I'm Frank Vukelić, and I'm a Bachelor of Science in Information Technology graduate." />
 
-        <meta property="og:title" content="John Carlo Devera | Frontend Developer" />
+        <meta property="og:title" content="Frank Vukelić | Web Developer" />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content="https://deverajc.com/" />
-        <meta property="og:image" content="https://deverajc.com/screenshot.png" />
-        <meta property="og:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate." />
+        {/* <meta property="og:url" content="https://deverajc.com/" /> */}
+        {/* <meta property="og:image" content="https://deverajc.com/screenshot.png" /> */}
+        <meta property="og:description" content="Hey! I'm Frank Vukelić, and I'm a Bachelor of Science in Information Technology graduate." />
 
+        {/* Twitter */}
         <meta name="twitter:card" content="summary"/>
         <meta name="twitter:site" content="@jc_devz"/>
-        <meta name="twitter:title" content="John Carlo Devera | Frontend Developer"/>
-        <meta name="twitter:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."/>
-        <meta name="twitter:creator" content="@jc_devz"/>
-        <meta name="twitter:image" content="https://deverajc.com/screenshot.png"/>
+        <meta name="twitter:title" content="Frank Vukelić | Web Developer"/>
+        <meta name="twitter:description" content="Hey! I'm Frank Vukelić, and I'm a Bachelor of Science in Information Technology graduate."/>
+        {/* <meta name="twitter:creator" content="@jc_devz"/> */}
+        {/* <meta name="twitter:image" content="https://deverajc.com/screenshot.png"/> */}
 
-        {/* <meta itemprop="name" content="John Carlo Devera | Frontend Developer"/>
-        <meta itemprop="description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."/>
+        {/* <meta itemprop="name" content="Frank Vukelić | Web Developer"/>
+        <meta itemprop="description" content="Hey! I'm Frank Vukelić, and I'm a Bachelor of Science in Information Technology graduate."/>
         <meta itemprop="image" content="https://deverajc.com/screenshot.png"/> */}
       </Head>
       <Home />
