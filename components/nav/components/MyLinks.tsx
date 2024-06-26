acb97cb72593b2a9041c8b22d18bfffe82004a9f
Jozef S <franck.vukelic@gmail.com>
2024-06-26 18:39:13 +0700

    Update MyLinks

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -3,8 +3,7 @@ import {
   AiFillLinkedin,
   AiFillGithub,
   AiFillInstagram,
-  AiFillYoutube,
-  AiFillSkype
+  AiOutlineWhatsApp,
 } from "react-icons/ai";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -12,55 +11,60 @@ import { motion } from "framer-motion";
 export const MyLinks = () => {
   return (
     <div className={styles.links}>
+      {/* WHATSAPP */}
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.0 }}
-      >
-        <Link href="https://www.linkedin.com/in/john-carlo-devera-5240761b6/" target="_blank" rel="nofollow">
-          <AiFillLinkedin size="2.4rem" />
-        </Link>
-      </motion.span>
-      
-      <motion.span
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
-        <Link href="https://join.skype.com/invite/vKVMgi5SGjvM" target="_blank" rel="nofollow">
-          <AiFillSkype size="2.4rem" />
+        transition={{ duration: 0.5, delay: 0.1 }}>
+        <Link
+          href="https://join.skype.com/invite/vKVMgi5SGjvM"
+          target="_blank"
+          rel="nofollow"
+          title="WhatsApp">
+          <AiOutlineWhatsApp size="2.4rem" />
         </Link>
       </motion.span>
 
+      {/* GITHUB */}
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >
-        <Link href="https://github.com/jcdevz-dev" target="_blank" rel="nofollow">
+        transition={{ duration: 0.5, delay: 0.2 }}>
+        <Link
+          href="https://github.com/basedhound"
+          target="_blank"
+          rel="nofollow"
+          title="GitHub">
           <AiFillGithub size="2.4rem" />
         </Link>
       </motion.span>
 
+      {/* LINKEDIN */}
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.3 }}
-      >
-        <Link href="https://www.instagram.com/aluckyfin/" target="_blank" rel="nofollow">
-          <AiFillInstagram size="2.4rem" />
+        transition={{ duration: 0.5, delay: 0.0 }}>
+        <Link
+          href="https://www.linkedin.com/in/frankdev/"
+          target="_blank"
+          rel="nofollow"
+          title="LinkedIn">
+          <AiFillLinkedin size="2.4rem" />
         </Link>
       </motion.span>
 
-      {/* <motion.span
+      {/* INSTAGRAM */}
+      <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-      >
-        <Link href="https://www.youtube.com/@aluckyfinsenpai2622" target="_blank" rel="nofollow">
-          <AiFillYoutube size="2.4rem" />
+        transition={{ duration: 0.5, delay: 0.3 }}>
+        <Link
+          href="https://www.instagram.com/aluckyfin/"
+          target="_blank"
+          rel="nofollow">
+          <AiFillInstagram size="2.4rem" />
         </Link>
-      </motion.span> */}
+      </motion.span>
     </div>
   );
 };
