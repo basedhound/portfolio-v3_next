aba88e12c0939c506191a094efba870a8073caf0
Jozef S <franck.vukelic@gmail.com>
2024-06-29 16:13:20 +0700

    Add Gallery Link

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { AiOutlineArrowRight } from "react-icons/ai";
 import { SectionHeader } from "@/components/utils/SectionHeader";
 import { Project } from "./Project";
 import styles from "./projects.module.scss";
@@ -7,11 +9,19 @@ export const Projects = () => {
     <section className="section-wrapper" id="projects">
       <SectionHeader title="Projects" dir="r" />
 
+    
+
       <div className={styles.projects}>
         {projects.map((project) => {
           return <Project key={project.title} {...project} />;
         })}
       </div>
+
+      <div className={styles.galleryLink}>
+        <AiOutlineArrowRight size="2rem" />
+        <Link href="/gallery">Access Full Gallery</Link>
+      </div>
+
     </section>
   );
 };
@@ -28,93 +38,22 @@ const projects = [
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
         </p>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
         </p>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
-        </p>
-      </>
-    ),
-  },
-  {
-    title: "Project",
-    imgSrc: "/projects/iphone.webp",
-    code: "https://www.github.com",
-    projectLink: "https://iphone15-fv.netlify.app/",
-    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
-    description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
-      modalContent: (
-        <>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
-          </p>
-        </>
-      ),
-  },
-  {
-    title: "Project",
-    imgSrc: "/projects/iphone.webp",
-    code: "https://www.github.com",
-    projectLink: "https://iphone15-fv.netlify.app/",
-    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
-    description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
-      modalContent: (
-        <>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
-          </p>
-        </>
-      ),
-  },
-  {
-    title: "Project",
-    imgSrc: "/projects/iphone.webp",
-    code: "https://www.github.com",
-    projectLink: "https://iphone15-fv.netlify.app/",
-    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
-    description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
-      modalContent: (
-        <>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
-          </p>
-        </>
-      ),
-  },
-  {
-    title: "Project",
-    imgSrc: "/projects/iphone.webp",
-    code: "https://www.github.com",
-    projectLink: "https://iphone15-fv.netlify.app/",
-    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
-    description:
-    "A Talent finder app for anything around the world. This is my first time as a React developer job.",
-      modalContent: (
-        <>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
-          </p>
-        </>
-      ),
-  },
-  {
-    title: "Project",
-    imgSrc: "/projects/iphone.webp",
-    code: "https://www.github.com",
-    projectLink: "https://iphone15-fv.netlify.app/",
-    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
-    description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
-    modalContent: (
-      <>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
         </p>
       </>
     ),
@@ -130,7 +69,10 @@ const projects = [
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
         </p>
       </>
     ),
@@ -146,7 +88,105 @@ const projects = [
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quisquam exercitationem aliquam delectus quas. Obcaecati aperiam, dignissimos expedita at quisquam nostrum commodi consequuntur ea, placeat magni maxime tenetur deleniti voluptates?
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Project",
+    imgSrc: "/projects/iphone.webp",
+    code: "https://www.github.com",
+    projectLink: "https://iphone15-fv.netlify.app/",
+    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
+    description:
+      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+    modalContent: (
+      <>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Project",
+    imgSrc: "/projects/iphone.webp",
+    code: "https://www.github.com",
+    projectLink: "https://iphone15-fv.netlify.app/",
+    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
+    description:
+      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+    modalContent: (
+      <>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Project",
+    imgSrc: "/projects/iphone.webp",
+    code: "https://www.github.com",
+    projectLink: "https://iphone15-fv.netlify.app/",
+    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
+    description:
+      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+    modalContent: (
+      <>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Project",
+    imgSrc: "/projects/iphone.webp",
+    code: "https://www.github.com",
+    projectLink: "https://iphone15-fv.netlify.app/",
+    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
+    description:
+      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+    modalContent: (
+      <>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Project",
+    imgSrc: "/projects/iphone.webp",
+    code: "https://www.github.com",
+    projectLink: "https://iphone15-fv.netlify.app/",
+    tech: ["React", "Nextjs", "Tailwind", "Prisma"],
+    description:
+      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+    modalContent: (
+      <>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
+          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
+          placeat magni maxime tenetur deleniti voluptates?
         </p>
       </>
     ),
