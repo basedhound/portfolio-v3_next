512e1c6b1c1bf3e114d66361676b2609e8c40f20
Jozef S <franck.vukelic@gmail.com>
2024-07-03 16:09:51 +0700

    Add MyLinks to Contact section

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -2,6 +2,7 @@ import { Reveal } from "@/components/utils/Reveal";
 import styles from "./contact.module.scss";
 import { AiFillMail } from "react-icons/ai";
 import Link from "next/link";
+import { MyLinks } from "../nav/MyLinks";
 
 export const Contact = () => {
   return (
@@ -14,28 +15,34 @@ export const Contact = () => {
         </Reveal>
         <Reveal width="100%">
           <p className={styles.contactCopy}>
-            Have an idea to discuss? Shoot me an email if you want to connect! You can also find me on{" "}
+            Have an idea to discuss? Shoot me an email if you want to connect!
+            You can also find me on{" "}
             <Link
               href="https://www.linkedin.com/in/frankdev/"
               target="_blank"
-              rel="nofollow"
-            >
+              rel="nofollow">
               LinkedIn
             </Link>{" "}
             or{" "}
-            <Link href="https://api.whatsapp.com/send?phone=33779134587" target="_blank" rel="nofollow">
+            <Link
+              href="https://api.whatsapp.com/send?phone=33779134587"
+              target="_blank"
+              rel="nofollow">
               WhatsApp
             </Link>{" "}
             if that&apos;s more your speed.
           </p>
         </Reveal>
         <Reveal width="100%">
-          <Link href="mailto:franck.vukelic@gmail.com">
-            <div className={styles.contactEmail}>
-              <AiFillMail size="2.4rem" />
-              <span>franck.vukelic@gmail.com</span>
-            </div>
+          <Link
+            href="mailto:franck.vukelic@gmail.com"
+            className={styles.contactEmail}>
+            <AiFillMail size="2.4rem" />
+            <span>franck.vukelic@gmail.com</span>
           </Link>
+        </Reveal>
+        <Reveal width="100%">
+          <MyLinks />
         </Reveal>
       </div>
     </section>
