ad1850338d0fd67a4037e3c881693376787db9ba
Jozef S <franck.vukelic@gmail.com>
2024-07-03 18:29:17 +0700

    Update Tech Stack

--- components/about/Stats.tsx
+++ components/about/Stats.tsx
@@ -9,24 +9,24 @@ export const Stats = () => {
         <div className={styles.statColumn}>
           <h4>
             <AiFillCode size="2.4rem" color="var(--brand)" />
-            <span>Main Stack</span>
+            <span>Tech Stack</span>
           </h4>
           <div className={styles.statGrid}>
+            <span className="chip">React</span>
+            <span className="chip">Next.js</span>
             <span className="chip">JavaScript</span>
             <span className="chip">TypeScript</span>
-            <span className="chip">HTML</span>
-            <span className="chip">CSS</span>
-            <span className="chip">Tailwind</span>
-            <span className="chip">React</span>
-            <span className="chip">Nextjs</span>
-            <span className="chip">Vuejs</span>
             <span className="chip">Redux</span>
-            <span className="chip">NodeJS</span>
+            <span className="chip">Tailwind</span>
+            <span className="chip">shadcn/ui</span>
+            <span className="chip">Bootstrap</span>
+            <span className="chip">Node.js</span>
             <span className="chip">Express</span>
-            <span className="chip">Postgres</span>
             <span className="chip">MongoDB</span>
+            <span className="chip">PostgreSQL</span>
             <span className="chip">Firebase</span>
-            <span className="chip">GitHub</span>
+            <span className="chip">GraphQL</span>
+            <span className="chip">Vercel</span>
           </div>
         </div>
       </Reveal>
@@ -34,20 +34,17 @@ export const Stats = () => {
         <div className={styles.statColumn}>
           <h4>
             <AiFillCode size="2.4rem" color="var(--brand)" />
-            <span>Use for fun</span>
+            <span>More</span>
           </h4>
           <div className={styles.statGrid}>
-            <span className="chip">React Native</span>
-            <span className="chip">Gatsby</span>
-            <span className="chip">Chakra Ui</span>
-            <span className="chip">Tailwind</span>
+            <span className="chip">Git</span>
+            <span className="chip">GitHub</span>
+            <span className="chip">Copywriting</span>
+            <span className="chip">SEO</span>
             <span className="chip">Figma</span>
-            <span className="chip">Planetscale</span>
-            <span className="chip">GraphQL</span>
-            <span className="chip">Firebase</span>
-            <span className="chip">Supabase</span>
-            <span className="chip">Vercel</span>
-            <span className="chip">Laravel</span>
+            <span className="chip">Photoshop</span>
+            <span className="chip">React Native</span>
+            <span className="chip">Expo</span>
           </div>
         </div>
       </Reveal>
