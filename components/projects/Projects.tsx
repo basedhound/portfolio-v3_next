9ecfeb8d17584a32230b99d55c7268da5010e33c
Jozef S <franck.vukelic@gmail.com>
2024-07-07 15:36:25 +0700

    Update typo

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -37,7 +37,7 @@ const projects = [
     modalContent: (
       <>
         <p>
-          This is a clone of Apple's iPhone 15 Pro website using React.js and
+          This is a clone of Apple&apos;s iPhone 15 Pro website using React.js and
           TailwindCSS. It highlights the effective use of GSAP (Greensock
           Animations) and Three.js for displaying iPhone 15 Pro models in
           various colors and shapes.
