8535c3641bd19b797c0e09ef34e0ad455adfb749
Jozef S <franck.vukelic@gmail.com>
2024-07-07 16:13:10 +0700

    Update projects descriptions and modal content

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -37,8 +37,8 @@ const projects = [
     modalContent: (
       <>
         <p>
-          This is a clone of Apple&apos;s iPhone 15 Pro website using React.js and
-          TailwindCSS. It highlights the effective use of GSAP (Greensock
+          This is a clone of Apple&apos;s iPhone 15 Pro website using React.js
+          and TailwindCSS. It highlights the effective use of GSAP (Greensock
           Animations) and Three.js for displaying iPhone 15 Pro models in
           various colors and shapes.
         </p>
@@ -129,14 +129,13 @@ const projects = [
     link: "https://travel-2-fv.netlify.app",
     tech: ["HTML", "CSS", "Sass", "JavaScript", "Figma"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "Landing page design for a travel agency featuring a modern and minimalist aesthetic, crafted using HTML, CSS, and JavaScript.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          Landing page design for a travel agency featuring a modern and
+          minimalist aesthetic, crafted using HTML, CSS, and JavaScript.
+          Enhanced with smooth animations powered by ScrollReveal.js library.
         </p>
       </>
     ),
@@ -148,14 +147,13 @@ const projects = [
     link: "https://sushi-2-fv.netlify.app",
     tech: ["HTML", "CSS", "Sass", "JavaScript", "Figma"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "Landing page for a sushi restaurant featuring a modern and minimalist aesthetic, crafted using HTML, CSS, and JavaScript.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          Landing page design for a sushi restaurant featuring a modern and
+          minimalist aesthetic, crafted using HTML, CSS, and JavaScript.
+          Enhanced with smooth animations powered by ScrollReveal.js library.
         </p>
       </>
     ),
@@ -167,14 +165,62 @@ const projects = [
     link: "https://imaginify-fv.vercel.app",
     tech: ["Next.js", "TypeScript", "MongoDB", "Tailwind", "Stripe"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "AI image platform featuring advanced image processing: restoration, recoloring, object removal, generative filling, and background removal.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          An AI image SaaS platform with advanced image processing, secure
+          payments and powerful search. Features: restoration, recoloring,
+          object removal, generative filling, and background removal.
+        </p>
+        <p>
+          ✔ Authentication and Authorization: Secure user access with
+          registration, login, and route protection.
+        </p>
+        <p>
+          ✔ Community Image Showcase: Explore user transformations with easy
+          navigation using pagination.
+        </p>
+        <p>
+          ✔ Advanced Image Search: Find images by content or objects present
+          inside the image quickly and accurately.
+        </p>
+        <p>✔ Image Restoration: Revive old or damaged images effortlessly.</p>
+        <p>
+          ✔ Image Recoloring: Customize images by replacing objects with desired
+          colors easily.
+        </p>
+        <p>
+          ✔ Image Generative Fill: Fill in missing areas of images seamlessly.
+        </p>
+        <p>
+          ✔ Object Removal: Clean up images by removing unwanted objects with
+          precision.
+        </p>
+        <p>✔ Background Removal: Extract objects from backgrounds with ease.</p>
+        <p>
+          ✔ Download Transformed Images: Save and share AI-transformed images
+          conveniently.
+        </p>
+        <p>
+          ✔ Transformed Image Details: View details of transformations for each
+          image.
+        </p>
+        <p>
+          ✔ Transformation Management: Control over deletion and updates of
+          transformations.
+        </p>
+        <p>
+          ✔ Profile Page: Access transformed images and credit information
+          personally.
+        </p>
+        <p>
+          ✔ Credits Purchase: Securely buy credits via Stripe for uninterrupted
+          use.
+        </p>
+        <p>
+          ✔ Responsive UI/UX: A seamless experience across devices with a
+          user-friendly interface.
         </p>
       </>
     ),
@@ -186,14 +232,46 @@ const projects = [
     link: "https://car-hub-fv.vercel.app",
     tech: ["Next.js", "React", "TypeScript", "Tailwind"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "CarHub presents various car types, showcasing comprehensive information with advanced filtering and pagination support.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          CarHub presents various car types, showcasing comprehensive
+          information with advanced filtering and pagination support. Built with
+          Next.js and leveraging its server-side rendering capabilities.
+        </p>
+        <p>
+          ✔ Home Page: Showcases a visually appealing display of cars fetched
+          from a third-party API, providing a captivating introduction to the
+          diverse range of vehicles available.
+        </p>
+        <p>
+          ✔ Exploration and Filtering: Explore a wide variety of cars from
+          around the world, utilizing a search and filter system based on
+          criteria such as model, manufacturer, year, fuel type, and make.
+        </p>
+        <p>
+          ✔ Transition to Server-Side Rendering: A seamless transition from
+          client-side rendering to server-side rendering, enhancing performance
+          and providing a smoother browsing experience.
+        </p>
+        <p>
+          ✔ Pagination: For easy navigation through a large dataset of cars,
+          allowing users to explore multiple pages effortlessly.
+        </p>
+        <p>
+          ✔ Metadata Optimization and SEO: Optimize metadata for car listing,
+          enhancing search engine optimization (SEO) and ensuring better
+          visibility on search engine results pages.
+        </p>
+        <p>
+          ✔ TypeScript Types: Utilize TypeScript to provide robust typing for
+          enhanced code quality and better development.
+        </p>
+        <p>
+          ✔ Responsive Website Design: The website is designed to be visually
+          pleasing and responsive, ensuring an optimal user experience across
+          various devices.
         </p>
       </>
     ),
@@ -205,14 +283,29 @@ const projects = [
     link: "https://portfolio-3d-island-fv.netlify.app",
     tech: ["React", "Three.js", "Three Fiber", "Tailwind", "Vite"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "3D website powered by Three.js featuring interactive elements. Navigate aboard a small plane and explore a floating island.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          This 3D website, built with React and powered by Three.js, features
+          engaging interactive elements. Navigate aboard a small plane to
+          explore a floating island and discover various information. On the
+          contact page, a fox will dynamically react as you fill out the fields!
+        </p>
+        <p>
+          ✔ Three.js is a React binding to Three.js, enabling developers to
+          integrate powerful 3D graphics and visualizations into React
+          applications seamlessly.
+        </p>
+        <p>
+          ✔ Three/Drei is a collection of useful helpers and abstractions for
+          React Three.js applications. It provides components like effects,
+          shaders, and pre-built 3D objects to simplify development.
+        </p>
+        <p>
+          ✔ Three/Fiber optimizes the React binding for Three.js, utilizing
+          React's reconciliation algorithm (Fiber) to efficiently manage and
+          update the 3D scene graph.
         </p>
       </>
     ),
@@ -224,14 +317,56 @@ const projects = [
     link: "https://podcastr-fv.vercel.app",
     tech: ["OpenAI", "Next.js", "React", "TypeScript", "Tailwind"],
     description:
-      "A Talent finder app for anything around the world. This is my first time as a React developer job.",
+      "AI platform that enables users to create podcasts with features like text-to-audio conversion, image generation and seamless playback.",
     modalContent: (
       <>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
-          quisquam exercitationem aliquam delectus quas. Obcaecati aperiam,
-          dignissimos expedita at quisquam nostrum commodi consequuntur ea,
-          placeat magni maxime tenetur deleniti voluptates?
+          A cutting-edge AI SaaS platform that empowers users to create,
+          discover, and enjoy podcasts with advanced features such as
+          text-to-audio conversion using multi-voice AI powered by OpenAI API,
+          podcast thumbnail image generation, and seamless playback. The
+          platform includes secure authentication using Clerk, ensuring a safe
+          and personalized user experience.
+        </p>
+        <p>
+          ✔ Robust Authentication: Secure and reliable user login and
+          registration system.
+        </p>
+        <p>
+          ✔ Modern Home Page: Showcases trending podcasts with a sticky podcast
+          player for continuous listening.
+        </p>
+        <p>
+          ✔ Discover Podcasts Page: Dedicated page for users to explore new and
+          popular podcasts.
+        </p>
+        <p>
+          ✔ Fully Functional Search: Allows users to find podcasts easily using
+          various search criteria.
+        </p>
+        <p>
+          ✔ Create Podcast Page: Enables podcast creation with text-to-audio
+          conversion, AI image generation, and previews.
+        </p>
+        <p>
+          ✔ Multi Voice AI Functionality: Supports multiple AI-generated voices
+          for dynamic podcast creation.
+        </p>
+        <p>
+          ✔ Profile Page: View all created podcasts with options to delete them.
+        </p>
+        <p>
+          ✔ Podcast Details Page: Displays detailed information about each
+          podcast, including creator details, number of listeners, and
+          transcript.
+        </p>
+        <p>
+          ✔ Podcast Player: Features backward/forward controls, as well as
+          mute/unmute functionality for a seamless listening experience.
+        </p>
+        <p>
+          ✔ Responsive Design: Fully functional and visually appealing across
+          all devices and screen sizes.
         </p>
       </>
     ),
