1493aeee2451d9098ea275dcb9176c2f2ed3d2ab
Jozef S <franck.vukelic@gmail.com>
2024-08-13 10:52:43 +0700

    Clean code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
-// import { SideBar } from "@/components/nav/SideBar";
-// import styles from "@/components/home.module.scss";
 
 const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div id="root" className={poppins.className}>
-      {/* <div className={styles.home}> */}
-        {/* <SideBar /> */}
         <Component {...pageProps} />
-      {/* </div> */}
     </div>
   );
 }
