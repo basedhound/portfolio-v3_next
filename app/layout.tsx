04db2764fc46b68ab30840f4a332535ff541d952
Jozef S <franck.vukelic@gmail.com>
2024-08-14 21:27:32 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -7,6 +7,9 @@ const poppins = Poppins({
   weight: ["100", "200", "400", "500", "600", "700", "900"],
   variable: "--font-poppins",
 });
+
+export const viewport: Viewport = {
+}
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://devfrank.vercel.app"),
