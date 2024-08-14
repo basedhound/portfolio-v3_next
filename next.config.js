745f045f917251d8fcd0bdb97fe8157640b5ebac
Jozef S <franck.vukelic@gmail.com>
2024-08-14 18:31:32 +0700

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
+/*   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'fr'
-  },
+  }, */
   experimental: {
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
   },
 };
 
