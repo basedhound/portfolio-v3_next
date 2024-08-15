add8829ea97bdab610eebed39e6d84d182febec6
Jozef S <franck.vukelic@gmail.com>
2024-08-15 16:34:11 +0700

    FIx

--- navigation.ts
+++ navigation.ts
@@ -1,7 +1,7 @@
+// @ts-nocheck
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 import { locales } from "./i18n";
 
-// @ts-ignore
 export const { Link, redirect, usePathname, useRouter } =
 createSharedPathnamesNavigation({locales, /* ... */});
 
