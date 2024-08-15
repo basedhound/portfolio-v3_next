bd97f3f37f731fdcc6e425ffaf8567cb2e1d64be
Jozef S <franck.vukelic@gmail.com>
2024-08-15 16:51:05 +0700

    Update

--- navigation.ts
+++ navigation.ts
@@ -2,6 +2,6 @@ import { createSharedPathnamesNavigation } from "next-intl/navigation";
 import { locales } from "./i18n";
 
 export const { Link, redirect, usePathname, useRouter } =
-createSharedPathnamesNavigation();
+createSharedPathnamesNavigation({locales, /* ... */});
 
 //! When using i18n.ts, use this link instead of next/link to ensure proper routing.
