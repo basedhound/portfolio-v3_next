2962697b75a2567282f4f345470f6eacbe3989b1
Jozef S <franck.vukelic@gmail.com>
2024-08-21 18:28:09 +0700

    Format

--- components/nav/Switch.tsx
+++ components/nav/Switch.tsx
@@ -9,15 +9,19 @@ export const Switch = () => {
 
   return (
     <motion.span
-    initial={{ y: -30, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.5, delay: 0.6 }}>
-    <Link href={currentPath.includes("/en") ? currentPath.replace("/en", "/fr") : currentPath.replace("/fr", "/en")} className={styles.switch}>
-
-    {/* Button */}
-    {currentPath.includes("/fr") ? "FR" : "EN"}
-    </Link>
-  </motion.span>
-  )
-}
-
+      initial={{ y: -30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, delay: 0.6 }}>
+      <Link
+        href={
+          currentPath.includes("/en")
+            ? currentPath.replace("/en", "/fr")
+            : currentPath.replace("/fr", "/en")
+        }
+        className={styles.switch}>
+        {/* Button */}
+        {currentPath.includes("/fr") ? "FR" : "EN"}
+      </Link>
+    </motion.span>
+  );
+};
