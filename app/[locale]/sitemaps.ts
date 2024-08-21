817b4f22b0532318c44d0c9ce5a7629b8abf325d
Jozef S <franck.vukelic@gmail.com>
2024-08-21 18:35:17 +0700

    Update sitemaps

--- app/[locale]/sitemaps.ts
+++ app/[locale]/sitemaps.ts
@@ -5,25 +5,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
     {
-      url: `${baseUrl}`,
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
-    },
-    {
-      url: `${baseUrl}/en`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en`,
+          fr: `${baseUrl}/fr`,
+        },
+      },
     },
     {
       url: `${baseUrl}/en/gallery`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/fr`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
