ab2b26ddcc4a643a49719dda273a69153b228e50
Jozef S <franck.vukelic@gmail.com>
2024-08-21 20:51:09 +0700

    Update sitemap

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://devfrank.vercel.com";
 
-
   return [
     {
       url: `${baseUrl}/en`,
