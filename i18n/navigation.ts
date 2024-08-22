d670d2951269f4e0542df4d40f3d9a0900949b14
Jozef S <franck.vukelic@gmail.com>
2024-08-22 18:16:11 +0700

    Update navigation

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,6 +1,6 @@
 //! next-intl
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
-import { locales } from './i18n';
+import {localePrefix} from './i18n';
  
 export const {Link, redirect, usePathname, useRouter} =
-  createSharedPathnamesNavigation({locales, /* ... */});
+  createSharedPathnamesNavigation({localePrefix});
