a56c5fa0514ee8b27312eeeb2b0cb6ef2ad8ff94
Jozef S <franck.vukelic@gmail.com>
2024-08-22 17:20:58 +0700

    Update Switch

--- components/nav/Switch.tsx
+++ components/nav/Switch.tsx
@@ -1,24 +1,20 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import styles from "./switch.module.scss";
 
 export const Switch = () => {
-  const router = useRouter();
   const currentPath = usePathname();
+  const newPath = currentPath.includes("/fr")
+    ? currentPath.replace("/fr", "/en")
+    : currentPath.replace("/", "/fr");
 
   return (
     <motion.span
       initial={{ y: -30, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, delay: 0.6 }}>
-      <Link
-        href={
-          currentPath.includes("/en")
-            ? currentPath.replace("/en", "/fr")
-            : currentPath.replace("/fr", "/en")
-        }
-        className={styles.switch}>
+      <Link href={newPath} className={styles.switch}>
         {/* Button */}
         {currentPath.includes("/fr") ? "FR" : "EN"}
       </Link>
