08901d15004101258e1e025f19c59859716c5600
Jozef S <franck.vukelic@gmail.com>
2024-08-23 16:23:54 +0700

    Update

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -4,31 +4,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://devfrank.vercel.com";
 
   return [
-    //! EN
     {
       url: `${baseUrl}`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/gallery`,
+      url: `${baseUrl}/en`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
+      changeFrequency: 'monthly',
+      priority: 1,
     },
-    //! FR
     {
       url: `${baseUrl}/fr`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/en/gallery`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/fr/gallery`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
-  ];
+  ]
 }
