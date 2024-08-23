d4e877b28e9987c9589b760d7305ba148a6acab7
Jozef S <franck.vukelic@gmail.com>
2024-08-23 16:23:47 +0700

    Update

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -1,6 +1,5 @@
 //! next-intl
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
-// Use localePrefix instead of Locales (to manage "en" as default = url/)
 import {locales} from './i18n';
  
 export const {Link, redirect, usePathname, useRouter} =
