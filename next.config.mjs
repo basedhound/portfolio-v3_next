c2132e46fc59d05c13814cd122ef401bfe4357ec
Jozef S <franck.vukelic@gmail.com>
2024-08-29 16:10:11 +0700

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const withNextIntl= createNextIntlPlugin('./i18n/i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "build",   
+  output: "export", 
   reactStrictMode: true,
   experimental: {},
 };
