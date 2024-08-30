aff811077b66ce7c64d72d8a441236bcf68bf18b
Jozef S <franck.vukelic@gmail.com>
2024-08-30 23:41:50 +0700

    Update Facebook link

--- components/nav/MyLinks.tsx
+++ components/nav/MyLinks.tsx
@@ -60,7 +60,7 @@ export const MyLinks = () => {
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.4 }}>
-        <Link href="https://m.me/frank.vkc" target="_blank" rel="nofollow">
+        <Link href="https://m.me/fvukelic" target="_blank" rel="nofollow">
           <AiFillFacebook size="2.4rem" />
         </Link>
       </motion.span>
