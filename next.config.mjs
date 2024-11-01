33758cf03d0715facf1b14a54979b5e33ee0caaa
Jozef S <franck.vukelic@gmail.com>
2024-11-01 15:51:54 +0700

    Add CSP to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,38 @@
-import createNextIntlPlugin from 'next-intl/plugin'
-const withNextIntl= createNextIntlPlugin('./i18n/i18n.ts')
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin("./i18n/i18n.ts");
+
+// Content Security Policy
+// https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {},
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
