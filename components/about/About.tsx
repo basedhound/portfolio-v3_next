ad1d014e3e070c4bce88f4445d055f74c451b179
Jozef S <franck.vukelic@gmail.com>
2024-11-11 18:27:19 +0700

    Update translation liens links

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -42,7 +42,7 @@ export const About = () => {
           <Reveal>
             <div className={styles.links}>
               <div className={styles.linksText}>
-                <span>Links</span>
+                <span>{t("links")}</span>
                 <AiOutlineArrowRight />
               </div>
               <Socials />
