58b1a29a569abff5d1fe5bb9f6f8c450555b68f7
Jozef S <franck.vukelic@gmail.com>
2024-11-20 18:56:15 +0700

    Updat

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,7 +19,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://devfrank.vercel.app"),
+  metadataBase: new URL("https://fvukelic.com"),
   title: "Frank Vukelic",
   description:
     "Full-stack developer skilled in Next.js/React and a versatile tech stack, also offering WordPress site development for small businesses.",
@@ -31,12 +31,12 @@ export const metadata: Metadata = {
     title: "Frank Vukelić",
     description:
       "Full-stack developer skilled in Next.js/React and a versatile tech stack, also offering WordPress site development for small businesses.",
-    url: "https://devfrank.vercel.app",
+    url: "https://fvukelic.com",
     // siteName: "Frank Vukelic Portfolio",
     type: "website",
     images: [
       {
-        url: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/devfrank.vercel.app/Frank%20Vukeli%C4%87%20-%20Web%20Developer/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa9e8c408-5c62-4e7d-8d40-771e29523783.png%3Ftoken%3DRapK6uD2jQZDTLwq_T8cMtxJEiVnLvKIhJWTW69GI9o%26height%3D960%26width%3D1200%26expires%3D33259637381/og.png",
+        url: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/https://fvukelic.com/Frank%20Vukeli%C4%87%20-%20Web%20Developer/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa9e8c408-5c62-4e7d-8d40-771e29523783.png%3Ftoken%3DRapK6uD2jQZDTLwq_T8cMtxJEiVnLvKIhJWTW69GI9o%26height%3D960%26width%3D1200%26expires%3D33259637381/og.png",
         width: 1200,
         height: 630,
         alt: "Preview image Frank Vukelic Portfolio",
@@ -46,13 +46,13 @@ export const metadata: Metadata = {
   // Twitter
   twitter: {
     card: "summary_large_image",
-    site: "@frank_vkh",
+    site: "@fvkelic",
     title: "Frank Vukelic",
     description:
       "Full-stack developer skilled in Next.js/React and a versatile tech stack, also offering WordPress site development for small businesses.",
     images: [
       {
-        url: "https://ogcdn.net/6064b869-74ed-4eb9-b76c-0b701ffe7e6b/v4/devfrank.vercel.app/Frank%20Vukeli%C4%87%20-%20Web%20Developer/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2Fa9e8c408-5c62-4e7d-8d40-771e29523783.png%3Ftoken%3DRapK6uD2jQZDTLwq_T8cMtxJEiVnLvKIhJWTW69GI9o%26height%3D960%26width%3D1200%26expires%3D33259637381/og.png",
+        url: "",
         width: 1200,
         height: 630,
         alt: "Preview image Frank Vukelić Portfolio",
--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://devfrank.vercel.com";
+  const baseUrl = "https://fvukelic.com";
 
   return [
     {
